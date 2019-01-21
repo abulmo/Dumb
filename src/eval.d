eb6ba4e9a1e286f8b48eb454fb22e18ed64ff197
Delorme Richard <richard@asusn55s.home>
2019-01-21 11:40:52 +0100

    2019 (c) Simpler and faster code

--- src/eval.d
+++ src/eval.d
@@ -1,13 +1,13 @@
 /*
  * File eval.d
  * Evaluation function
- * © 2017-2018 Richard Delorme
+ * © 2017-2019 Richard Delorme
  */
 
 module eval;
 
 import board, move, util;
-import std.algorithm;
+import std.algorithm, std.range;
 
 enum Score {mate = 30_000, low = -29_000, high = 29_000, big = 3_000}
 
@@ -39,18 +39,15 @@ struct Value {
 
 /* Evaluation function */
 final class Eval {
-	struct Weight {
-		Value [Piece.size] material;
-		Value [Square.size][Piece.size] positional;
-		Value tempo;
-	}
 	struct Stack {
 		Value [Color.size] value;
 		int stage;
 	}
 	static immutable int [Piece.size] stageValue = [0, 0, 3, 3, 5, 10, 0];
-	Weight coeff;
 	Stack [Limits.ply.max + 1] stack;
+	Value [Square.size][Piece.size] weight;
+	Value tempo;
+	enum Adjustment {none, mean, min};
 	int ply;
 
 	static double attraction(const Square x, const Square y) {
@@ -60,100 +57,77 @@ final class Eval {
 		return d == 0 ? 2.0 : 1.0 / d;
 	}
 
-	static void adjustPawn(string phase) (ref Value [Square.size] p) {
-		foreach(x; Square.a1 .. Square.a2) mixin("p[x]."~phase) = 0;
-		foreach(x; Square.a8 .. Square.size) mixin("p[x]."~phase) = 0;
-		int m = int.max;
-		foreach(x; Square.a2 .. Square.a8) m = min(m, mixin("p[x]."~phase));
-		foreach(x; Square.a2 .. Square.a8) mixin("p[x]."~phase) -= m;
+	static void buildPositional(string phase)(ref Value [Square.size] w, const Square [] y, const double a, const Adjustment r) {
+		double m = 0.0, k;	
+		double [Square.size] p;
+ 	
+		foreach (s; allSquares) {
+			Square x = cast (Square) s;
+			k = attraction(x, y[0]); foreach (i; 1 .. y.length) k = max(attraction(x, y[i]), k); p[x] = a * k;
+		}
+
+		if (r == Adjustment.mean) {
+			foreach (x; allSquares) m += p[x]; m /= Square.size;
+		}
+
+		foreach(x; allSquares) mixin("w[x]." ~ phase) += cast (int) (p[x] - m);
+
+		if (r == Adjustment.min) {
+			int d = int.max; foreach(x; Square.a2 .. Square.a8) d = min(d, mixin("w[x]." ~ phase));
+			foreach(x; Square.a2 .. Square.a8) mixin("w[x]." ~ phase) -= d;
+		}
 	}
 
-	static void buildPositional(string phase)(ref Value [Square.size] positional, const Square [] y, const double a, const bool isPawn) {
-		double w;	
-		double [Square.size] p;
-	
-		foreach (Square x; Square.a1 .. Square.size) {
-			w = attraction(x, y[0]);
-			foreach (i; 1 .. y.length) w = max(attraction(x, y[i]), w);
-			p[x] = a * w;
-		}
-		if (!isPawn) {
-			double m = 0.0; foreach (x; Square.a1 .. Square.size) m += p[x]; m /= Square.size;
-			foreach (x; Square.a1 .. Square.size) p[x] -= m;
-		}
-		foreach(x; Square.a1 .. Square.size) mixin("positional[x]."~phase) += cast (int) p[x];
+	static void addBonus(string phase)(ref Value [Square.size] w, const Square [] squares, const int bonus) { 
+		foreach (x; squares) mixin("w[x]."~phase) += bonus;
 	}
 
 	void remove(const Piece p, const Color c, const Square x) {
-		Stack *s = &stack[ply];
-		s.value[c] -= coeff.positional[p][forward(x, c)] + coeff.material[p];
-		s.stage -= stageValue[p];
+		stack[ply].value[c] -= weight[p][forward(x, c)];
+		stack[ply].stage -= stageValue[p];
 	}
 
 	void set(const Piece p, const Color c, const Square x) {
-		Stack *s = &stack[ply];
-		s.value[c] += coeff.positional[p][forward(x, c)] + coeff.material[p];
-		s.stage += stageValue[p];
+		stack[ply].value[c] += weight[p][forward(x, c)];
+		stack[ply].stage += stageValue[p];
 	}
 
 	void deplace(const Piece p, const Color c, const Square from, const Square to) {
-		Stack *s = &stack[ply];
-		s.value[c] -= coeff.positional[p][forward(from, c)];
-		s.value[c] += coeff.positional[p][forward(to, c)];
+		stack[ply].value[c] += weight[p][forward(to, c)] - weight[p][forward(from, c)];
 	}
 
 	this() {
 		immutable int [] w = [
-			+100, +382, +404, +548, +1238, +2, +347, +96, +15, +108, +5, +47, -42, -41, -46, -9, -31, +25, +17,
-			+146, +356, +381, +685, +1163, +128, +47, +19, +46, +11, -7
+			+2, +347, +96, +15, +108, +5, +47, -42, -41, -46, -9, -31, +25, +100, +382, +404, +548, +1238, +17,
+			+128, +47, +19, +46, +11, +146, +356, +381, +685, +1163, -7,
 		];
-		
 		size_t i;
-		static immutable Square [] pawnCenter = [Square.d4, Square.e4];
-		static immutable Square [] pawnAdvance = [Square.b8, Square.c8, Square.d8, Square.e8, Square.f8, Square.g8];
-		static immutable Square [] knightOutpost = [Square.c6, Square.d6, Square.e6, Square.f6];
-		static immutable Square [] knightCenter = [Square.d4, Square.e4, Square.d5, Square.e5];
-		static immutable Square [] bishopCenter = [Square.c3, Square.f3, Square.c6, Square.f6];
-		static immutable Square [] rook7thRank = [Square.b7, Square.c7, Square.d7, Square.e7, Square.f7, Square.g7];
-		static immutable Square [] queenCenter = [Square.d4, Square.e4, Square.d5, Square.e5];
-		static immutable Square [] kingCastle = [Square.b1, Square.g1];
-		static immutable Square [] kingCenter = [Square.d4, Square.e4, Square.d5, Square.e5];
 
-		foreach(p; Piece.pawn .. Piece.king) coeff.material[p].opening = w[i++];
-		coeff.material[Piece.king].opening = 0;
+		with (Square) {
+			buildPositional!"opening"(weight[Piece.pawn],   [d4, e4],                 w[i++], Adjustment.none);
+			buildPositional!"opening"(weight[Piece.pawn],   [b8, c8, d8, e8, f8, g8], w[i++], Adjustment.min);
+			buildPositional!"opening"(weight[Piece.knight], [c6, d6, e6, f6],         w[i++], Adjustment.mean);
+			buildPositional!"opening"(weight[Piece.bishop], [c3, f3, c6, f6],         w[i++], Adjustment.mean);
+			buildPositional!"opening"(weight[Piece.rook],   [b7, c7, d7, e7, f7, g7], w[i++], Adjustment.mean);
+			buildPositional!"opening"(weight[Piece.queen],  [d4, e4, d5, e5],         w[i++], Adjustment.mean);
+			buildPositional!"opening"(weight[Piece.king],   [b1, g1],                 w[i++], Adjustment.mean);
+			addBonus!"opening"(weight[Piece.pawn],   [d2, e2], w[i++]) ;
+			addBonus!"opening"(weight[Piece.knight], [b1, g1], w[i++]) ;
+			addBonus!"opening"(weight[Piece.bishop], [c1, f1], w[i++]) ;
+			addBonus!"opening"(weight[Piece.rook],   [a1, h1], w[i++]) ;
+			weight[Piece.queen][d1].opening += w[i++];
+			weight[ Piece.king][e1].opening += w[i++];
+			foreach (p;  Piece.pawn .. Piece.king) addBonus!"opening"(weight[p], allSquares, w[i++]);
+			tempo.opening = w[i++];
 
-		buildPositional!"opening"(coeff.positional[Piece.pawn],   pawnCenter,    w[i++], true);
-		buildPositional!"opening"(coeff.positional[Piece.pawn],   pawnAdvance,   w[i++], true);
-		buildPositional!"opening"(coeff.positional[Piece.knight], knightOutpost, w[i++], false);
-		buildPositional!"opening"(coeff.positional[Piece.bishop], bishopCenter,  w[i++], false);
-		buildPositional!"opening"(coeff.positional[Piece.rook],   rook7thRank,   w[i++], false);
-		buildPositional!"opening"(coeff.positional[Piece.queen],  queenCenter,   w[i++], false);
-		buildPositional!"opening"(coeff.positional[Piece.king],   kingCastle,    w[i++], false);
-		adjustPawn!"opening"(coeff.positional[Piece.pawn]);
-		coeff.positional[Piece.pawn][Square.d2].opening   += w[i] ;
-		coeff.positional[Piece.pawn][Square.e2].opening   += w[i++];
-		coeff.positional[Piece.knight][Square.b1].opening += w[i];
-		coeff.positional[Piece.knight][Square.g1].opening += w[i++];
-		coeff.positional[Piece.bishop][Square.c1].opening += w[i];
-		coeff.positional[Piece.bishop][Square.f1].opening += w[i++];
-		coeff.positional[Piece.rook][Square.a1].opening   += w[i];
-		coeff.positional[Piece.rook][Square.h1].opening   += w[i++];
-		coeff.positional[Piece.queen][Square.d1].opening  += w[i++];
-		coeff.positional[Piece.king][Square.e1].opening   += w[i++];
-
-		coeff.tempo.opening = w[i++];
-
-		foreach(p; Piece.pawn .. Piece.king) coeff.material[p].endgame = w[i++];
-		coeff.material[Piece.king].endgame = 0;
-
-		buildPositional!"endgame"(coeff.positional[Piece.pawn],   pawnAdvance,  w[i++], true);
-		buildPositional!"endgame"(coeff.positional[Piece.knight], knightCenter, w[i++], false);
-		buildPositional!"endgame"(coeff.positional[Piece.bishop], bishopCenter, w[i++], false);
-		buildPositional!"endgame"(coeff.positional[Piece.queen],  queenCenter,  w[i++], false);
-		buildPositional!"endgame"(coeff.positional[Piece.king],   kingCenter,   w[i++], false);
-		adjustPawn!"endgame"(coeff.positional[Piece.pawn]);
-	
-		coeff.tempo.endgame = w[i++];
+			buildPositional!"endgame"(weight[Piece.pawn],   [b8, c8, d8, e8, f8, g8], w[i++], Adjustment.min);
+			buildPositional!"endgame"(weight[Piece.knight], [d4, e4, d5, e5],         w[i++], Adjustment.mean);
+			buildPositional!"endgame"(weight[Piece.bishop], [c3, f3, c6, f6],         w[i++], Adjustment.mean);
+			buildPositional!"endgame"(weight[Piece.queen],  [d4, e4, d5, e5],         w[i++], Adjustment.mean);
+			buildPositional!"endgame"(weight[Piece.king],   [d4, e4, d5, e5],         w[i++], Adjustment.mean);
+			foreach (p;  Piece.pawn .. Piece.king) addBonus!"endgame"(weight[p], allSquares, w[i++]);
+			tempo.endgame = w[i++];
+		}
 	}
 
 	void set(const Board board) {
@@ -163,15 +137,14 @@ final class Eval {
 		s.value[Color.white] = s.value[Color.black] = Value.init;
 		s.stage = 0;
 
-		foreach(Color c; Color.white .. Color.size) {
-			foreach(Piece p; Piece.pawn .. Piece.size) {
+		foreach(c; Color.white .. Color.size) {
+			foreach(p; Piece.pawn .. Piece.size) {
 				ulong b = board.color[c] & board.piece[p];
 				const n = countBits(b);
 				s.stage += stageValue[p] * n;
-				s.value[c] += coeff.material[p] * n;
 				while (b) {
 					const Square x = popSquare(b);
-					s.value[c] += coeff.positional[p][forward(x, c)];
+					s.value[c] += weight[p][forward(x, c)];
 				}
 			}
 		}
@@ -193,12 +166,12 @@ final class Eval {
 				remove(p, player, m.to);
 				set(m.promotion, player, m.to);
 			} else if (b.stack[b.ply - 1].enpassant == m.to) {
-				remove(Piece.pawn, enemy, toSquare(file(m.to), rank(m.from)));
+				remove(Piece.pawn, enemy, m.to.shift);
 			}
 		}
 		if (p == Piece.king) {
-			if (m.to == m.from + 2) deplace(Piece.rook, player, cast (Square) (m.from + 3), cast (Square) (m.from + 1));
-			if (m.to == m.from - 2) deplace(Piece.rook, player, cast (Square) (m.from - 4), cast (Square) (m.from - 1));
+			if (m.to == m.from + 2) deplace(Piece.rook, player, m.from.shift(+3), m.from.shift(+1));
+			if (m.to == m.from - 2) deplace(Piece.rook, player, m.from.shift(-4), m.from.shift(-1));
 		}
 	}
 
@@ -206,7 +179,7 @@ final class Eval {
 
 	int opCall(const Board b) const {
 		const Stack *s = &stack[ply];
-		const Value value = s.value[b.player] - s.value[opponent(b.player)] + coeff.tempo;
+		const Value value = s.value[b.player] - s.value[opponent(b.player)] + tempo;
 
 		return (value.opening * s.stage + value.endgame * (64 - s.stage)) / 64;
 	}
