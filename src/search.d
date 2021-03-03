71f0c6d57a1b35e2f9835c5f1d941c3d19b27ccf
Richard Delorme <edax.reversi@gmail.com>
2021-03-03 12:02:32 +0100

    Remove all pruning methods & transpositions methods to stick to plain alphabeta + quiescence search

--- src/search.d
+++ src/search.d
@@ -10,68 +10,6 @@ import board, eval, move, util;
 import std.stdio, std.string, std.format, std.algorithm, std.math;
 import core.bitop;
 
-/* Hash table score bound */
-enum Bound {none, upper, lower, exact}
-
-/* Entry Table Entry */
-struct Entry {
-	ulong code;
-	ushort info;
-	Move move;
-	short value;
-
-	int depth() const @property { return info >> 2;	}
-
-	Bound bound() const @property { return cast (Bound) (info & 3);	}
-
-	int score(const int ply) const { return value < Score.low ? value + ply : (value > Score.high ? value - ply : value); }
-
-	void set(const Key k, const int d, const int ply, const Bound b, const int v, const Move m) {
-		code = k.code;
-		info = cast (ushort) (b | (d << 2));
-		value = cast (short) (v < Score.low ? v - ply : (v > Score.high ? v + ply : v));
-		move = m;
-	}
-}
-
-/* Transposition table */
-final class TranspositionTable {
-	enum size_t bucketSize = 4;
-	Entry [] entry;
-	size_t mask;
-
-	this(size_t size) { resize(size); }
-
-	void resize(size_t size) {
-		mask = (1 << bsf(size / Entry.sizeof)) - 1;
-		entry.length = mask + bucketSize;
-		clear();
-	}
-
-	void clear() { foreach (ref h; entry) h = Entry.init; }
-
-	bool probe(const Key k, ref Entry found) {
-		const size_t i = cast (size_t) (k.code & mask);
-		foreach (ref h; entry[i .. i + bucketSize]) if (h.code == k.code) {
-			found = h;
-			return true;
-		}
-		return false;
-	}
-
-	void store(const Key k, const int depth, const int ply, const Bound b, const int v, const Move m) @trusted {
-		const size_t i = cast (size_t) (k.code & mask);
-		Entry *w = &entry[i];
-		foreach (ref h; entry[i .. i + bucketSize]) {
-			if (h.code == k.code) return h.set(k, depth, ply, b, v, m);
-			else if (w.info > h.info) w = &h;
-		}
-		w.set(k, depth, ply, b, v, m);
-	}
-
-	Bound bound(const int v, const int β) const { return v >= β ? Bound.lower : Bound.exact; }
-}
-
 /* Search Option */
 struct Option {
 	struct Time { double max; } 
@@ -87,25 +25,14 @@ struct Option {
 final class Search {
 	Board board;
 	Eval eval;
-	TranspositionTable tt;
-	History history;
 	Moves rootMoves;
 	shared Event event;
 	Option option;
 	Chrono timer;
 	Line [Limits.ply.max + 1] pv;
-	Move [2][Limits.ply.max + 2] killer;
-	ubyte [32][32] reduction;
-	int [Limits.ply.max + 1] sv;
 	ulong pvsNodes, qsNodes;
 	int ply, score;
 	bool stop;
-
-	this(size_t size = 64 * 1024 * 1024) {
-		tt = new TranspositionTable(size);
-		foreach (d; 1 .. 32)
-		foreach (m; 1 .. 32) reduction[d][m] = cast (ubyte) (1.1 * std.math.log(d) + 0.7 * std.math.log(m));
-	}
 
 	bool checkTime(const double timeMax) const { return option.isPondering || timer.time < timeMax; }
 
@@ -147,14 +74,6 @@ final class Search {
 		--ply;
 	}
 
-	int reduce(const int d, const int m) const { return reduction[min(d, 31)][min(m, 31)]; }
-
-	void store(Move m, const int d, const ref Moves moves) {
-		if (m != killer[ply][0]) { killer[ply][1] = killer[ply][0]; killer[ply][0] = m; }
-		history.update(board, m, d * d, history.good);
-		for (int k = 0; m != moves[k]; ++k) history.update(board, moves[k], d * d, history.bad);
-	}
-
 	int qs(int α, int β) {
 		int s, bs; 
 		Moves moves = void;
@@ -162,23 +81,21 @@ final class Search {
 		Move m;
 
 		++qsNodes;
+		if (pvsNodes + qsNodes >= option.nodes.max) { stop = true; return α; }
 
 		if (board.isDraw) return 0;
 
-		bs = ply - Score.mate;
-		s = Score.mate - ply - 1;
-		if (s < β && (β = s) <= α) return α;
-
+		
 		if (!board.inCheck) {
 			bs = eval(board);
 			if ((bs > α) && (α = bs) >= β) return bs;
-		}
+		} else bs = ply - Score.mate;
 
 		if (ply == Limits.ply.max) return eval(board);
 
-		moves.generate!false(board, history);
+		moves.generate!false(board);
 
-		while ((m = (i = moves.next).move) != 0) if (board.inCheck || i.isTactical) {
+		while ((m = (i = moves.next).move) != 0) {
 			update!false(m);
 				s = -qs(-β, -α);
 			restore(m);
@@ -188,13 +105,12 @@ final class Search {
 		return bs;
 	}
 
-	int pvs(int α, int β, const int d) {
-		const bool isPv = (α + 1 < β);
-		int e, r, s, bs, v, IIR = 0, quiet = 0;
+	int αβ(int α, int β, const int d) {
+		const bool isPv = (β > α + 1);
+		int s, bs = ply - Score.mate;
 		Moves moves = void;
 		MoveItem i = void;
 		Move m;
-		Entry h;
 
 		pv[ply].clear();
 
@@ -205,72 +121,20 @@ final class Search {
 
 		if (board.isDraw) return 0;
 
-		bs = ply - Score.mate;
-		s = Score.mate - ply - 1;
-		if (s < β && (β = s) <= α) return α;
-
-		if (tt.probe(board.key, h) && !isPv) {
-			s = h.score(ply);
-			if (h.depth >= d || s <= ply - Score.mate || s >= Score.mate - ply - 1) {
-				if (h.bound == Bound.exact) return s;
-				else if (h.bound == Bound.lower && s >= β) return s;
-				else if (h.bound == Bound.upper && s <= α) return s;
-			}
-		}
-
-		v = sv[ply] = eval(board);
-		if (ply == Limits.ply.max) return v;
-
-		const bool tactical = (board.inCheck || abs(v) >= Score.big || α >= Score.big || β <= -Score.big);
-		const bool suspicious = (isPv || (ply >= 2 && sv[ply] > sv[ply - 2]));
-
-		if (!tactical && !isPv) {
-			const δ = 200 * d - 100;
-			if (v >= β + δ) return β;
-			if (v <= α - δ) {
-				if (d <= 2) return qs(α, β);
-				else if (qs(α - δ, α - δ + 1) <= α - δ) return α;
-			}
-
-			if (v >= β && (board.color[board.player] & ~(board.piece[Piece.pawn] | board.piece[Piece.king]))) {
-				r = 3 + d / 4 + min((v - β) / 128, 3);
-				update(0);
-					s = -pvs(-β, -β + 1, d - r);
-				restore(0);
-				if (!stop && s >= β) {
-					if (s >= Score.high) s = β;
-					tt.store(board.key, d, ply, Bound.lower, s, h.move);
-					return s;
-				}
-			}
-		}
-
-		IIR = (h.move == 0);
+		if (ply == Limits.ply.max) return α;
 
 		if (ply == 0) moves = rootMoves;
-		else moves.generate(board, history, h.move, killer[ply]);
+		else moves.generate(board);
 
 		const αOld = α;
 
 		while ((m = (i = moves.next).move) != 0) {
 			update(m);
-				e = board.inCheck;
-				if (moves.isFirst(m)) s = -pvs(-β, -α, d + e - 1);
-				else {
-					r = (i.isTactical || e || tactical) ? 0 : reduce(d, ++quiet) + IIR;
-					if (r && suspicious) --r;
-					if (r && quiet > (4 + d * d) / (2 - suspicious)) s = bs;
-					else {
-						s = -pvs(-α - 1, -α, d + e - r - 1);
-						if ((r && s > bs) || (α < s && s < β)) s = -pvs(-β, -α, d + e - 1);
-					}
-				}
+				s = -αβ(-β, -α, d - 1);
 			restore(m);
 			if (stop) break;
 			if (s > bs && (bs = s) > α) {
 				if (ply == 0) rootMoves.setBest(m, 0);
-				else tt.store(board.key, d, ply, tt.bound(bs, β), bs, m);
-				if (!i.isTactical) store(m, d, moves);
 				if (isPv) pv[ply].set(m, pv[ply + 1]);
 				if ((α = bs) >= β) break;
 			}
@@ -281,44 +145,18 @@ final class Search {
 			else return 0;
 		}
 
-		if (!stop && bs <= αOld) tt.store(board.key, d, ply, Bound.upper, bs, moves[0]);
-
 		return bs;
 	}
 
-	void aspiration(const int α, const int β, const int d) {
-		int λ, υ, δ = +10;
-
-		if (d <= 4) {
-			score = pvs(α, β, d);
-		} else for (λ = score - δ, υ = score + δ; !stop; δ *= 2) {
-			λ = max(α, λ); υ = min(β, υ);
-			score = pvs(λ, υ, d);
-			if      (score <= λ && λ > α) { υ = (λ + υ) / 2; λ = score - δ; }
-			else if (score >= υ && υ < β) { λ = (λ + υ) / 2; υ = score + δ; }
-			else break;
-		}
-		writeUCI(d);
-	}
-
 	bool persist(const int d) const { return !stop && checkTime(0.7 * option.time.max) && d <= option.depth.max && pvsNodes + qsNodes < option.nodes.max; }
-
-	void clear() {
-		tt.clear();
-		foreach (k; killer) k[] = 0;
-		history = History.init;
-	}
-
-	void resize(const size_t size) { tt.resize(size); }
 
 	Move bestMove() const @property { return rootMoves[0]; }
 
 	Move hint() const @property { return pv[0].n > 1 ? pv[0].move[1] : 0; }
 
 	void set() {
-		rootMoves.generate(board, history);
+		rootMoves.generate(board);
 		eval.set(board);
-		history.scale(8);
 	}
 
 	void go(const ref Option o, const ref Moves moves) {
@@ -329,7 +167,10 @@ final class Search {
 		stop = false;
 		if (moves.length > 0) rootMoves = moves;
 		if (rootMoves.length == 0) rootMoves.push(0);
-		else for (int d = 1; persist(d); ++d) aspiration(-Score.mate, Score.mate, d);
+		else for (int d = 1; persist(d); ++d) { 
+			score = αβ(-Score.mate, Score.mate, d);
+			writeUCI(d);
+		}
 	}
 }
 
